8529b7a32864bbc1fa129662896a006129469db1
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-01-20 23:39:52 +0100

    feat: :sparkles: set hash function (not sure if needed)

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,7 @@ const config = {
         filename: '[name].js',
         libraryTarget: 'commonjs2',
         devtoolModuleFilenameTemplate: '../[resource-path]',
+        hashFunction: 'sha256',
     },
 
     devtool: 'source-map',
